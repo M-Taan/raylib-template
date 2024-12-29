ce5b9856a3599d7d509d6f86073bac6ec647f4ab
Mohamad Taan <mohamadmtaan@gmail.com>
2024-12-29 21:44:12 +0200

    run binary optionally

--- build.sh
+++ build.sh
@@ -2,14 +2,21 @@
 
 set -eu
 
+should_run=${1:-no}
+
 RAYLIB_PATH="raylib"
 
 if [ ! -f "$RAYLIB_PATH/src/libraylib.a" ]; then
     sudo pacman -S --needed alsa-lib mesa libx11 libxrandr libxi libxcursor libxinerama base-devel git libxkbcommon
     cd "$RAYLIB_PATH/src"
     make USE_WAYLAND_DISPLAY=TRUE PLATFORM=PLATFORM_DESKTOP
+    cd -
 fi
 
 gcc main.c -o main -std=c99 -Wall -I. -I./raylib/src -I./raylib/src/external -lraylib -lm -ldl -pthread
+
+if [ "$should_run" = "run" ]; then
+    ./main
+fi
 
 set +eu
